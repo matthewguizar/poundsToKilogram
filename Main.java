d11e9cbbfd0b7a83f36fb6c1555832dd726e08b6
Matthew Guizar <matthewrguizar@gmail.com>
2022-09-09 16:50:46 -0700

    added toMilesPerHour method

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     final static double kiloConversion = 0.45359237;
     public static void main(String[] args) {
         pound2Kilo(200);
+        printConversion(25.42);
         
     }
 
@@ -13,5 +14,23 @@ public class Main {
 
     public static void pound2Kilo(double pounds){
         System.out.println(converter(pounds));
+    }
+
+    public static long toMilesPerHour(double kilometerPerHour){
+        double result;
+        if (kilometerPerHour < 0){
+            result = -1;
+        } else {
+           result = (kilometerPerHour / 1.609);
+        }
+        return Math.round(result);
+    }
+
+    public static void printConversion(double kilometerPerHour){
+        if (kilometerPerHour < 0)
+        System.out.println("Invalid Value");
+        else {
+            System.out.println(kilometerPerHour + " km/h = " + toMilesPerHour(kilometerPerHour) +" mi/h");
+        }
     }
 }
